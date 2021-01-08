74ec9cfa533f76a622c66a3581856efd33757ca8
h2sm <68076283+h2sm@users.noreply.github.com>
2021-01-08 18:40:32 +0300

    Create second.java

---
+++ second.java
@@ -0,0 +1,34 @@
+package threads.basic;
+
+import java.sql.Time;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+public class second {
+    public static void main(String[] args) throws InterruptedException {
+        var threads = new ArrayList<Thread>();
+        for (int i = 0; i < 10; i++) threads.add(new Thread(new Worker(i)));
+        threads.forEach(Thread::start);
+    }
+
+}
+class Worker implements Runnable{
+    private long timeStart = System.currentTimeMillis();//время запуска треда
+    int numberOfThread;
+
+    public Worker(int numThread){
+        this.numberOfThread=numThread;
+    }
+    @Override
+    public void run() {
+        while (! Thread.currentThread().isInterrupted()) {
+            long timeEnd = System.currentTimeMillis();
+            System.out.println("Thread number " + numberOfThread + ". Elapsed time is : " + (float) (timeEnd - timeStart) / 1000F);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
