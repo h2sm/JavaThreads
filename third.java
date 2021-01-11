925214d3e748e86ec84847bec99196a9f5bc45fa
h2sm <68076283+h2sm@users.noreply.github.com>
2021-01-11 19:03:37 +0300

    Update third.java

--- third.java
+++ third.java
@@ -4,27 +4,44 @@ import java.util.ArrayList;
 public class third {
     public static void main(String[] args) throws InterruptedException {
         var threads = new ArrayList<Thread>();
-        final long timeStart = System.currentTimeMillis();
         for (int i = 0; i < 10; i++) threads.add(new Thread(new newWorker(i)));
         threads.forEach(Thread::start);
 
-        }
+        threads.forEach(third::join);
+        System.out.println("main finished");
+
     }
+    private static void join(Thread t) {
+        try { t.join(); }
+        catch (Exception e) { /*nothing*/ }
+    }
+}
 
 class newWorker implements Runnable{
     int numberOfThread;
+    private boolean isActive;
+    final private long timeStart = System.currentTimeMillis();//время запуска треда
+    private int counter = 0;
+
+    void disable(){
+        isActive=false;
+    }
 
     public newWorker(int numThread){
         this.numberOfThread=numThread;
+        isActive=true;
     }
     @Override
     public void run() {
-        int k =0;
         try {
-            while(k!=5) {
-                k++;
-                System.out.println("hello from thread " + numberOfThread + " I working at " + k + " seconds");
+            while(isActive) {
+                counter++;
+                long timeEnd = System.currentTimeMillis();
+                System.out.println("hello from thread " + numberOfThread + " working for " + (float) (timeEnd - timeStart) / 1000F);
                 Thread.sleep(1000);
+                if (counter==5){
+                    disable();
+                }
             }
             System.out.println(Thread.currentThread().getName() + " interrupted");
         }
