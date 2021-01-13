96ee7030f84d879ef490f2fdcc0ee148d83e4d02
h2sm <68076283+h2sm@users.noreply.github.com>
2021-01-13 16:52:21 +0300

    Update sixth.java

--- sixth.java
+++ sixth.java
@@ -3,38 +3,60 @@
 package threads.basic;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Random;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class sixth {
     public static void main(String[] args) throws InterruptedException {
-        ReentrantLock reentrantLock = new ReentrantLock();
         var producersThreads = new ArrayList<Thread>();
         var consumersThreads = new ArrayList<Thread>();
         for (int i = 0; i < 10; i++) {
-            producersThreads.add(new Thread(new Producers(reentrantLock)));
-            consumersThreads.add(new Thread(new Consumers(reentrantLock)));
+            producersThreads.add(new Thread(new Producers()));
+            consumersThreads.add(new Thread(new Consumers()));
         }
 
         producersThreads.forEach(Thread::start);
         consumersThreads.forEach(Thread::start);
 
-//        Thread.sleep(4000);
+        //Thread.sleep(10000);
 
-        producersThreads.forEach(Thread::interrupt);
-        consumersThreads.forEach(Thread::interrupt);
+//        producersThreads.forEach(Thread::interrupt);
+//        consumersThreads.forEach(Thread::interrupt);
     }
 
 }
 class BufferFile{
-    static int[] SHAREDMEMORY = new int[1];
-    void addSmth(){//добавляем в буфер
-        SHAREDMEMORY[0]++;
-        showSum("add");
+    static ReentrantLock reentrantLock = new ReentrantLock();
+    static Condition condition;
+    BufferFile(){
+        condition= reentrantLock.newCondition();
     }
-    void retrieveSmth(){//забираем из буфера
-        SHAREDMEMORY[0]--;
-        showSum("retrieve");
+    static int[] SHAREDMEMORY = new int[1];
+    void add(){//добавляем в буфер
+        reentrantLock.lock();
+        try {
+            while (SHAREDMEMORY[0]!=0) condition.await();
+            SHAREDMEMORY[0]++;
+            showSum("add");
+            condition.signal();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            reentrantLock.unlock();
+        }
+    }
+    void retrieve(){//забираем из буфера
+        reentrantLock.lock();
+        try {
+            while (SHAREDMEMORY[0]!=1) condition.await();
+            SHAREDMEMORY[0]--;
+            showSum("retrieve");
+            condition.signal();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            reentrantLock.unlock();
+        }
     }
     void showSum(String str){
         System.out.println(Arrays.toString(SHAREDMEMORY) + " from " + str);
@@ -45,40 +67,20 @@ class BufferFile{
 }
 
 class Producers implements Runnable{
-    ReentrantLock reentrantLock;
     BufferFile bufferFile = new BufferFile();
-    public Producers(ReentrantLock rl){
-        reentrantLock=rl;
-    }
     @Override
     public void run() {
-        reentrantLock.lock();
-        try {
-            while (bufferFile.sum()<1)
-                bufferFile.addSmth();
-        }
-        finally {
-            reentrantLock.unlock();
-        }
-
+        bufferFile.add();
+        System.out.println(Thread.currentThread().getName());
     }
 }
 class Consumers implements Runnable{
-    ReentrantLock lock;
     BufferFile bufferFile = new BufferFile();
-    public Consumers(ReentrantLock reentrantLock){
-        lock=reentrantLock;
-    }
     @Override
     public void run() {
-        lock.lock();
-        try {
-            while (bufferFile.sum()>0)
-                bufferFile.retrieveSmth();
-        }
-        finally {
-            lock.unlock();
-        }
+        bufferFile.retrieve();
+        System.out.println(Thread.currentThread().getName());
+
     }
 }
 
