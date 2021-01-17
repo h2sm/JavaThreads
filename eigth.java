ef6071442e9b177e91bc7b9342dece661ea6ca5e
h2sm <68076283+h2sm@users.noreply.github.com>
2021-01-17 14:47:16 +0300

    Update eigth.java

--- eigth.java
+++ eigth.java
@@ -1,5 +1,6 @@
 package threads.basic;
 import java.util.ArrayList;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class eight {
     public static void main(String[] args) {
@@ -12,21 +13,32 @@ public class eight {
     }
 }
 class PiCount{
-    private double PI = 0;
-    void countPI(){
-        for (int i = 1; i < 1000000000; i += 4) {
-            PI += 8.0 / (i * (i + 2L));
+    static ReentrantLock reentrantLock = new ReentrantLock();
+    static int i = 1;
+    static int EPS = 1000000000;
+    static private double PI = 0;
+    static void countPI(){
+        reentrantLock.lock();
+        try {
+            if (i<EPS) {
+                PI += 8.0 / (i * (i + 2L));
+                i+=4;
+            }
+            System.out.println(PI);
         }
-        System.out.println(PI);
+        finally {
+            reentrantLock.unlock();
+        }
     }
 }
 
 class Counter implements Runnable{
-PiCount piCount = new PiCount();
+    PiCount piCount = new PiCount();
     @Override
     public void run() {
-        piCount.countPI();
-        System.out.println(Thread.currentThread().getName());
+        while (PiCount.i != PiCount.EPS) {
+            piCount.countPI();
+            System.out.println(Thread.currentThread().getName() +  " " + PiCount.i + " i " + PiCount.EPS + " eps");
+        }
     }
 }
-
