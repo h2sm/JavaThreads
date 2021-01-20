9af01dd666c46fef09424f88bc57aa3dc72501e3
h2sm <68076283+h2sm@users.noreply.github.com>
2021-01-20 15:41:38 +0300

    Update seventh.java

--- seventh.java
+++ seventh.java
@@ -3,11 +3,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 public class seventh {
     public static void main(String[] args) {
+        BufferSeven bufferSeven = new BufferSeven();
         var producerThread = new ArrayList<Thread>();
         var consumerThread = new ArrayList<Thread>();
         for (int i = 0; i < 10; i++) {
-            producerThread.add(new Thread(new ProducerSeven()));
-            consumerThread.add(new Thread(new ConsumerSeven()));
+            producerThread.add(new Thread(new ProducerSeven(bufferSeven)));
+            consumerThread.add(new Thread(new ConsumerSeven(bufferSeven)));
         }
 
         producerThread.forEach(Thread::start);
@@ -20,35 +21,45 @@ public class seventh {
     }
 
 }
-class BufferSeven{
-    private static int[] SHAREDMEMORY = new int[1];
-    public void addSmth() throws InterruptedException {//добавляем в буфер
-        synchronized (SHAREDMEMORY){
-            while (SHAREDMEMORY[0]!=1)
-            SHAREDMEMORY[0]++;
-        }
-        showSum("add");
-    }
-    public void retrieveSmth() throws InterruptedException {//забираем из буфера
-        synchronized (SHAREDMEMORY){
-            while (SHAREDMEMORY[0]!=0)
-            SHAREDMEMORY[0]--;
-        }
-        showSum("retrieve");
-    }
-    private void showSum(String str){
-        System.out.println(Arrays.toString(SHAREDMEMORY) + " from " + str);
-    }
+class BufferSeven{//представим, что sharedmemory увеличивается от 0 до 3
+    private int SHAREDMEMORY = 0;
+    synchronized public void addSmth() throws InterruptedException {//добавляем в буфер
+       while (SHAREDMEMORY>=3){
+           try {
+               wait();
+           }
+           catch (InterruptedException e) {
+           }
+       }
+        SHAREDMEMORY++;
+        System.out.println("Added " + SHAREDMEMORY);
+        notify();
 
+    }
+    synchronized public void retrieveSmth() throws InterruptedException {//забираем из буфера
+        while (SHAREDMEMORY<1){
+            try {
+                wait();
+            }
+            catch (InterruptedException e) {
+            }
+        }
+        SHAREDMEMORY--;
+        System.out.println("Retrieved " + SHAREDMEMORY);
+         notify();
+    }
 }
 
 class ProducerSeven implements Runnable{
-    BufferSeven bufferSeven = new BufferSeven();
+    BufferSeven buf;
+    public ProducerSeven(BufferSeven bufferSeven){
+        this.buf = bufferSeven;
+    }
     @Override
     public void run() {
         while (!Thread.currentThread().isInterrupted()) {
             try {
-                bufferSeven.addSmth();
+                buf.addSmth();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -56,12 +67,15 @@ class ProducerSeven implements Runnable{
     }
 }
 class ConsumerSeven implements Runnable{
-    BufferSeven bufferSeven = new BufferSeven();
+    BufferSeven buf;
+    public ConsumerSeven(BufferSeven bufferSeven){
+        this.buf=bufferSeven;
+    }
     @Override
     public void run() {
         while (!Thread.currentThread().isInterrupted()) {
             try {
-                bufferSeven.retrieveSmth();
+                buf.retrieveSmth();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
